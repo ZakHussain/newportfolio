4b22ec04542ff963539ef81f93c4fb5e0f2d5858
Ubuntu <ubuntu@ip-172-31-10-197.us-west-1.compute.internal>
2021-10-29 09:54:49 +0000

    made all updates to deploy

--- personalPortfolio/personalPortfolio/settings.py
+++ personalPortfolio/personalPortfolio/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&#ip893)&@!ryqk=$lqd0fp!6a@-h##^sfr2cqfm-&4+3z$p(#'
+with open('/home/ubuntu/k.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.215.108.220', 'zakhussain.com', 'www.zakhussain.com', 'ec2-54-215-108-220.us-west-1.compute.amazonaws.com', 'localhost']
 
 
 # Application definition
